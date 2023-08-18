574a076de84aebcc535bfd3f67bab7a385c046fd
ismailoguzcanarslan <ismailoguzcanarslan@gmail.com>
2023-08-19 02:45:27 +0300

    cache remove operation

---
+++ Core.Application/PipeLines/Caching/CacheRemovingBehavior.cs
@@ -0,0 +1,38 @@
+﻿using MediatR;
+using Microsoft.Extensions.Caching.Distributed;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Application.PipeLines.Caching;
+
+public class CacheRemovingBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : IRequest<TResponse>, ICacheRemoverRequest
+{
+
+    private readonly IDistributedCache _cache;
+
+    public CacheRemovingBehavior(IDistributedCache cache)
+    {
+        _cache = cache;
+    }
+
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        if (request.ByPassCache)
+        {
+            return await next();
+        }
+
+        TResponse response = await next();
+
+        if (string.IsNullOrEmpty(request.CacheKey))
+        {
+            await _cache.RemoveAsync(request.CacheKey);
+        }
+
+        return response;
+    }
+}
